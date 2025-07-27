811abf5b26f2adb58dea2b67a2bb772b04fc0d91
divneev15 <divneevsingh123@gmail.com>
2025-07-27 16:49:57 +0530

    Create even_test.cpp

---
+++ even_test.cpp
@@ -0,0 +1,26 @@
+//Divneev singh 24070123043 entc A2
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout <<"Enter a number ";
+cin >> n;
+if (n%2==0)
+{
+    cout<<"This is an even number";
+
+}
+else
+{
+    cout<<"This is an Odd number";
+}
+}
+
+/*
+Output:
+Enter a number 6
+This is an even number
+Enter a number 9
+This is an Odd number
+*/
